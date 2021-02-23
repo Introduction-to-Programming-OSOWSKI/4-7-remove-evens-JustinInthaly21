346e9f00fe739787282364bffa53b57821e6da3e
JustinInthaly21 <825134@mystma.org>
2021-02-23 20:16:03 +0000

    removeevens 1

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#WRITE YOUR CODE IN THIS FILE
+def removeEvens(k):
+    g = k
+    h = 0
+    for i in range(0, len(k)):
+        if g[i - h] % 2 == 0 and g[i - h] != 0:
+            g.pop(i - h)
+            h = h + 1
+
+    return g
+            
